575c1ce442230a57f2fa2bf4eaa81723401894bf
tomatagolo <tom@agolo.com>
2016-07-04 13:36:19 -0400

    enable empty sections

--- GooglePlacesAutocomplete.js
+++ GooglePlacesAutocomplete.js
@@ -134,7 +134,7 @@ const GooglePlacesAutocomplete = React.createClass({
 
   buildRowsFromResults(results) {
     var res = null;
-    
+
     if (results.length === 0 || this.props.predefinedPlacesAlwaysVisible === true) {
       res = [...this.props.predefinedPlaces];
       if (this.props.currentLocation === true) {
@@ -146,14 +146,14 @@ const GooglePlacesAutocomplete = React.createClass({
     } else {
       res = [];
     }
-    
+
     res = res.map(function(place) {
       return {
         ...place,
         isPredefinedPlace: true,
       }
     });
-    
+
     return [...res, ...results];
   },
 
@@ -176,13 +176,13 @@ const GooglePlacesAutocomplete = React.createClass({
     if (this.refs.textInput) this.refs.textInput.focus();
   },
 
-  /**   
-   * This method is exposed to parent components to blur textInput manually.   
-   * @public   
-   */    
+  /**
+   * This method is exposed to parent components to blur textInput manually.
+   * @public
+   */
   triggerBlur() {
     if (this.refs.textInput) this.refs.textInput.blur();
-  },   
+  },
 
   getCurrentLocation() {
     navigator.geolocation.getCurrentPosition(
@@ -198,8 +198,8 @@ const GooglePlacesAutocomplete = React.createClass({
   },
 
   _enableRowLoader(rowData) {
-    
-    let rows = this.buildRowsFromResults(this._results);    
+
+    let rows = this.buildRowsFromResults(this._results);
     for (let i = 0; i < rows.length; i++) {
       if ((rows[i].place_id === rowData.place_id) || (rows[i].isCurrentLocation === true && rowData.isCurrentLocation === true)) {
         rows[i].isLoading = true;
@@ -274,11 +274,11 @@ const GooglePlacesAutocomplete = React.createClass({
       }));
       request.send();
     } else if (rowData.isCurrentLocation === true) {
-      
+
       // display loader
       this._enableRowLoader(rowData);
-      
-      
+
+
       this.setState({
         text: rowData.description,
       });
@@ -287,7 +287,7 @@ const GooglePlacesAutocomplete = React.createClass({
       delete rowData.isLoading;
 
       this.getCurrentLocation();
-      
+
     } else {
       this.setState({
         text: rowData.description,
@@ -296,16 +296,16 @@ const GooglePlacesAutocomplete = React.createClass({
       this._onBlur();
 
       delete rowData.isLoading;
-      
+
       let predefinedPlace = this._getPredefinedPlace(rowData);
-      
+
       // sending predefinedPlace as details for predefined places
       this.props.onPress(predefinedPlace, predefinedPlace);
     }
   },
   _results: [],
   _requests: [],
-  
+
   _getPredefinedPlace(rowData) {
     if (rowData.isPredefinedPlace !== true) {
       return rowData;
@@ -317,10 +317,10 @@ const GooglePlacesAutocomplete = React.createClass({
     }
     return rowData;
   },
-  
+
   _filterResultsByTypes(responseJSON, types) {
     if (types.length === 0) return responseJSON.results;
-    
+
     var results = [];
     for (let i = 0; i < responseJSON.results.length; i++) {
       let found = false;
@@ -336,8 +336,8 @@ const GooglePlacesAutocomplete = React.createClass({
     }
     return results;
   },
-  
-  
+
+
   _requestNearby(latitude, longitude) {
     this._abortRequests();
     if (latitude !== undefined && longitude !== undefined && latitude !== null && longitude !== null) {
@@ -351,9 +351,9 @@ const GooglePlacesAutocomplete = React.createClass({
         }
         if (request.status === 200) {
           const responseJSON = JSON.parse(request.responseText);
-          
+
           this._disableRowLoaders();
-          
+
           if (typeof responseJSON.results !== 'undefined') {
             if (this.isMounted()) {
               var results = [];
@@ -362,7 +362,7 @@ const GooglePlacesAutocomplete = React.createClass({
               } else {
                 results = responseJSON.results;
               }
-              
+
               this.setState({
                 dataSource: this.state.dataSource.cloneWithRows(this.buildRowsFromResults(results)),
               });
@@ -375,7 +375,7 @@ const GooglePlacesAutocomplete = React.createClass({
           // console.warn("google places autocomplete: request could not be completed or has been aborted");
         }
       };
-      
+
       let url = '';
       if (this.props.nearbyPlacesAPI === 'GoogleReverseGeocoding') {
         // your key must be allowed to use Google Maps Geocoding API
@@ -391,7 +391,7 @@ const GooglePlacesAutocomplete = React.createClass({
           ...this.props.GooglePlacesSearchQuery,
         });
       }
-      
+
       request.open('GET', url);
       request.send();
     } else {
@@ -402,9 +402,9 @@ const GooglePlacesAutocomplete = React.createClass({
     }
   },
 
-  
-  
-  
+
+
+
   _request(text) {
     this._abortRequests();
     if (text.length >= this.props.minLength) {
@@ -449,7 +449,7 @@ const GooglePlacesAutocomplete = React.createClass({
       listViewDisplayed: true,
     });
   },
-  
+
   _getRowLoader() {
     if (Platform.OS === 'android') {
       return (
@@ -466,7 +466,7 @@ const GooglePlacesAutocomplete = React.createClass({
       />
     );
   },
-  
+
   _renderLoader(rowData) {
     if (rowData.isLoading === true) {
       return (
@@ -474,15 +474,15 @@ const GooglePlacesAutocomplete = React.createClass({
           style={[defaultStyles.loader, this.props.styles.loader]}
         >
           {this._getRowLoader()}
-        </View>      
-      );      
+        </View>
+      );
     }
     return null;
   },
 
   _renderRow(rowData = {}) {
     rowData.description = rowData.description || rowData.formatted_address || rowData.name;
-    
+
     return (
       <TouchableHighlight
         onPress={() =>
@@ -519,6 +519,7 @@ const GooglePlacesAutocomplete = React.createClass({
     if ((this.state.text !== '' || this.props.predefinedPlaces.length || this.props.currentLocation === true) && this.state.listViewDisplayed === true) {
       return (
         <ListView
+          enableEmptySections={true}
           keyboardShouldPersistTaps={true}
           keyboardDismissMode="on-drag"
           style={[defaultStyles.listView, this.props.styles.listView]}
